dd363a90c5262c91741dc23f5885b266a949a7b5
Majid-Razzaq <abdulmajid22770@gmail.com>
2024-09-04 09:46:10 +0500

    update code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -13,6 +14,55 @@ class UserController extends Controller
             'message' => count($users). ' users found',
             'data' => $users,
             'status' => true,
-        ]);
+        ], 200);
     }
+
+    // find specific user using this function
+    public function show($id){
+      
+        $user = User::find($id);
+
+        if($user != null){
+            return response()->json([
+                'message' => $user->name.' User found',
+                'data' => $user,
+                'status' => true,
+            ], 200);
+        }else{
+            return response()->json([
+                'message' => 'Record not found',
+                'status' => true,
+                'data' => [],
+            ], 200);
+
+        }
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'message' => 'Please fix the errors',
+                'errors' => $validator->errors(),
+                'status' => false,
+            ], 200);
+        }
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+
+        return response()->json([
+            'message' => 'User added successfully',
+            'status' => true,
+            'data' => $user,
+        ], 200);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,5 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/demo',[HomeController::class,'index']);
 Route::get('/users',[UserController::class,'index']);
+Route::get('/users/{id}',[UserController::class,'show']);
+Route::post('/users',[UserController::class,'store']);
